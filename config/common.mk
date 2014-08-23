e26e0f4ea0ffe47077460eef9a63c5c7cf30c430
Quarx2k <agent00791@gmail.com>
2014-08-23 18:52:07 +0700

    Add AeroRom option

--- config/common.mk
+++ config/common.mk
@@ -248,7 +248,7 @@ ifndef CM_BUILDTYPE
 endif
 
 # Filter out random types, so it'll reset to UNOFFICIAL
-ifeq ($(filter RELEASE NIGHTLY SNAPSHOT EXPERIMENTAL,$(CM_BUILDTYPE)),)
+ifeq ($(filter RELEASE AeroRom NIGHTLY SNAPSHOT EXPERIMENTAL,$(CM_BUILDTYPE)),)
     CM_BUILDTYPE :=
 endif
 
