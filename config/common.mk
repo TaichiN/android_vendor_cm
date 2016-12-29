87efd26573792bc4a7344ec7d984ecf2c0801710
fuzz <fuzzzzzzzzzzzzzzzzzz@gmail.com>
2016-12-29 19:14:44 +0900

    cm: Rebranding SKANK

--- config/common.mk
+++ config/common.mk
@@ -287,7 +287,7 @@ ifdef CM_BUILDTYPE
     endif
 else
     # If CM_BUILDTYPE is not defined, set to UNOFFICIAL
-    CM_BUILDTYPE := UNOFFICIAL
+    CM_BUILDTYPE := SKANKY
     CM_EXTRAVERSION :=
 endif
 
